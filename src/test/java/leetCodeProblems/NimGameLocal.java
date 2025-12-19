cae2a78c34860690360e8edffd2fc2ebfd19f100
ashokadi34 <ashokadi34@gmail.com>
2025-12-19 22:10:21 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/NimGameLocal.java
@@ -0,0 +1,33 @@
+package leetCodeProblems;
+
+import java.util.Scanner;
+
+public class NimGameLocal {
+
+    // method
+    public static boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+    public static void main(String[] args) {
+        // Fixed tests (examples)
+        System.out.println("Fixed tests:");
+        System.out.println("n = 4  -> " + canWinNim(4));  // false
+        System.out.println("n = 1  -> " + canWinNim(1));  // true
+        System.out.println("n = 2  -> " + canWinNim(2));  // true
+        System.out.println("n = 7  -> " + canWinNim(7));  // true
+        System.out.println("n = 8  -> " + canWinNim(8));  // false
+
+        // Interactive mode
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nInteractive mode (type a number, or 'q' to quit):");
+        while (true) {
+            System.out.print("Enter n: ");
+            if (!sc.hasNextInt()) break;   // non-int -> exit
+            int n = sc.nextInt();
+            System.out.println("canWinNim(" + n + ") = " + canWinNim(n));
+        }
+        sc.close();
+        System.out.println("Bye!");
+    }
+}
