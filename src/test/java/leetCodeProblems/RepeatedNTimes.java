0e50dcd2939a8cb82f174495514627da922058da
ashokadi34 <ashokadi34@gmail.com>
2026-01-02 08:33:42 +0530

    done small changes

--- src/test/java/leetCodeProblems/RepeatedNTimes.java
+++ src/test/java/leetCodeProblems/RepeatedNTimes.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class RepeatedNTimes {
+	//functionality
     public int repeatedNTimes(int[] nums) {
         Set<Integer> seen = new HashSet<>();
         
