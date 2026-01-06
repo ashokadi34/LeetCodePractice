739786be5d36930d975a762e0fbc3237c5a7e7dc
ashokadi34 <ashokadi34@gmail.com>
2026-01-06 23:42:00 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/FindTheDifference.java
@@ -0,0 +1,27 @@
+package leetCodeProblems;
+
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        char res = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            res ^= s.charAt(i);
+        }
+        
+        for (int i = 0; i < t.length(); i++) {
+            res ^= t.charAt(i);
+        }
+        
+        return res;
+    }
+    
+    // Local testing
+    public static void main(String[] args) {
+        FindTheDifference sol = new FindTheDifference();
+        
+        System.out.println(sol.findTheDifference("abcd", "abcde")); // e
+        System.out.println(sol.findTheDifference("", "y"));         // y
+        System.out.println(sol.findTheDifference("a", "aa"));       // a
+        System.out.println(sol.findTheDifference("ae", "aea"));     // a or e depending on extra
+    }
+}
