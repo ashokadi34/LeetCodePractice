b1cc123ff1ac855f25081734f785d211ed7b984e
ashokadi34 <ashokadi34@gmail.com>
2026-01-10 15:18:25 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/ToHex.java
@@ -0,0 +1,32 @@
+package leetCodeProblems;
+
+public class ToHex {
+    public String toHex(int num) {
+        if (num == 0) return "0";
+        
+        char[] digits = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        StringBuilder sb = new StringBuilder();
+        
+        while (num != 0) {
+            sb.append(digits[num & 0xf]);
+            num >>>= 4;
+        }
+        return sb.reverse().toString();
+    }
+
+    // Local testing in Eclipse
+    public static void main(String[] args) {
+        ToHex sol = new ToHex();
+        
+        // Example 1
+        System.out.println(sol.toHex(26));     // "1a"
+        
+        // Example 2
+        System.out.println(sol.toHex(-1));     // "ffffffff"
+        
+        // Additional tests
+        System.out.println(sol.toHex(0));      // "0"
+        System.out.println(sol.toHex(15));     // "f"
+        System.out.println(sol.toHex(-2147483648));  // "80000000" (min int)
+    }
+}
