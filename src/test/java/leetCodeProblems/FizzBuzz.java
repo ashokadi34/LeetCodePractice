9a582fabd5f2974b20fe9834a07ceac1e5e3a2e6
ashokadi34 <ashokadi34@gmail.com>
2026-01-13 23:46:43 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/FizzBuzz.java
@@ -0,0 +1,35 @@
+package leetCodeProblems;
+
+import java.util.*;
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                ans.add("Fizz");
+            } else if (i % 5 == 0) {
+                ans.add("Buzz");
+            } else {
+                ans.add(String.valueOf(i));
+            }
+        }
+        return ans;
+    }
+
+    // Local testing
+    public static void main(String[] args) {
+    	FizzBuzz sol = new FizzBuzz();
+        
+        // Example 1: n = 3
+        System.out.println(sol.fizzBuzz(3));
+        
+        // Example 2: n = 5
+        System.out.println(sol.fizzBuzz(5));
+        
+        // Example 3: n = 15 (first few)
+        System.out.println(sol.fizzBuzz(15));
+    }
+}
