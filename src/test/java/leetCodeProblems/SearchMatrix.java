78033338be09d3ea925f1bae2d61b68c4d10ec33
ashokadi34 <ashokadi34@gmail.com>
2026-03-12 22:42:51 +0530

    done small changes

--- src/test/java/leetCodeProblems/SearchMatrix.java
+++ src/test/java/leetCodeProblems/SearchMatrix.java
@@ -21,6 +21,7 @@ class SearchMatrix {
         return false;
     }
     
+    //Test cases
     public static void main(String[] args) {
     	SearchMatrix s = new SearchMatrix();
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
