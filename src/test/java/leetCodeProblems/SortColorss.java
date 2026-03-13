e6dcb0a4a1b358cde60c767f2c6e26075e4af718
ashokadi34 <ashokadi34@gmail.com>
2026-03-13 19:13:32 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/SortColorss.java
@@ -0,0 +1,37 @@
+package leetCodeProblems;
+
+import java.util.Arrays;
+
+class SortColorss {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public static void main(String[] args) {
+    	SortColorss sol = new SortColorss();
+        int[] nums = {2,0,2,1,1,0};
+        sol.sortColors(nums);
+        System.out.println(Arrays.toString(nums)); // [0,0,1,1,2,2]
+    }
+
+}
+
