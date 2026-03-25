1f2b21f19037b531ca7a2d08a0276769ea2361e5
ashokadi34 <ashokadi34@gmail.com>
2026-03-25 22:14:07 +0530

    done small changes

--- src/test/java/leetCodeProblems/EqualSumGridPartitionI.java
+++ src/test/java/leetCodeProblems/EqualSumGridPartitionI.java
@@ -55,6 +55,7 @@ public class EqualSumGridPartitionI {
         System.out.println("]");
     }
 
+    //Test cases
     public static void main(String[] args) {
         Solution sol = new Solution();
 
