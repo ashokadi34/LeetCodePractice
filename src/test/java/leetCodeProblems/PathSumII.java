de3f1c3cb9c89590e4b0113c57f521d33244a15a
ashokadi34 <ashokadi34@gmail.com>
2026-04-14 20:38:55 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/PathSumII.java
@@ -0,0 +1,53 @@
+package leetCodeProblems;
+
+import java.util.*;
+
+public class PathSumII {
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(root, targetSum, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, int remaining, List<Integer> path, List<List<Integer>> result) {
+        if (node == null) return;
+
+        path.add(node.val);
+        remaining -= node.val;
+
+        if (node.left == null && node.right == null && remaining == 0) {
+            result.add(new ArrayList<>(path));
+        } else {
+            dfs(node.left, remaining, path, result);
+            dfs(node.right, remaining, path, result);
+        }
+
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+    	PathSumII sol = new PathSumII();
+
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.left = new TreeNode(5);
+        root.right.right.right = new TreeNode(1);
+
+        List<List<Integer>> ans = sol.pathSum(root, 22);
+        System.out.println(ans); // [[5, 4, 11, 2], [5, 8, 4, 5]]
+    }
+}
