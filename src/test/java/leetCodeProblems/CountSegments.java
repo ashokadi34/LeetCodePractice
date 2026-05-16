28924acaebb009263eb65e1f0adb74694f690041
ashokadi34 <ashokadi34@gmail.com>
2026-05-16 12:34:51 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/CountSegments.java
@@ -0,0 +1,20 @@
+package leetCodeProblems;
+
+public class CountSegments {
+    public static int countSegments(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countSegments("Hello, my name is John")); // 5
+        System.out.println(countSegments("Hello"));                  // 1
+        System.out.println(countSegments(""));                       // 0
+        System.out.println(countSegments("   a  b   "));             // 2
+    }
+}
