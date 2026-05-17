94fc4a2d522801fecd4c40a903eec49f97bf8a88
ashokadi34 <ashokadi34@gmail.com>
2026-05-17 11:29:01 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/JumpGameIII.java
@@ -0,0 +1,24 @@
+package leetCodeProblems;
+
+public class JumpGameIII {
+    public boolean canReach(int[] arr, int start) {
+        boolean[] visited = new boolean[arr.length];
+        return dfs(arr, start, visited);
+    }
+
+    private boolean dfs(int[] arr, int i, boolean[] visited) {
+        if (i < 0 || i >= arr.length || visited[i]) return false;
+        if (arr[i] == 0) return true;
+
+        visited[i] = true;
+        return dfs(arr, i + arr[i], visited) || dfs(arr, i - arr[i], visited);
+    }
+
+    public static void main(String[] args) {
+        JumpGameIII sol = new JumpGameIII();
+
+        System.out.println(sol.canReach(new int[]{4,2,3,0,3,1,2}, 5)); // true
+        System.out.println(sol.canReach(new int[]{4,2,3,0,3,1,2}, 0)); // true
+        System.out.println(sol.canReach(new int[]{3,0,2,1,2}, 2));     // false
+    }
+}
