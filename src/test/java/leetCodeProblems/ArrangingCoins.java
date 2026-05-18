a9d7275cb16f0b98c00ac8e0bad7717f90979f2e
ashokadi34 <ashokadi34@gmail.com>
2026-05-18 21:50:04 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/ArrangingCoins.java
@@ -0,0 +1,25 @@
+package leetCodeProblems;
+
+public class ArrangingCoins {
+    public int arrangeCoins(int n) {
+        long left = 0, right = n;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long coins = mid * (mid + 1) / 2;
+
+            if (coins == n) return (int) mid;
+            if (coins < n) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return (int) right;
+    }
+
+    public static void main(String[] args) {
+        ArrangingCoins sol = new ArrangingCoins();
+        System.out.println(sol.arrangeCoins(5));  // 2
+        System.out.println(sol.arrangeCoins(8));  // 3
+        System.out.println(sol.arrangeCoins(1));  // 1
+    }
+}
