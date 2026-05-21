cfe04c14af51c737607aea0f37dc660559b5b0a5
ashokadi34 <ashokadi34@gmail.com>
2026-05-21 22:40:39 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/LongestCommonPrefix2.java
@@ -0,0 +1,43 @@
+package leetCodeProblems;
+
+import java.util.*;
+
+public class LongestCommonPrefix2 {
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr1) {
+            while (num > 0) {
+                set.add(num);
+                num /= 10;
+            }
+        }
+
+        int ans = 0;
+
+        for (int num : arr2) {
+            while (num > 0) {
+                if (set.contains(num)) {
+                    ans = Math.max(ans, String.valueOf(num).length());
+                    break;
+                }
+                num /= 10;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix2 sol = new LongestCommonPrefix2();
+
+        int[] arr1 = {1, 10, 100};
+        int[] arr2 = {1000};
+        System.out.println(sol.longestCommonPrefix(arr1, arr2)); // 3
+
+        int[] arr3 = {1, 2, 3};
+        int[] arr4 = {4, 4, 4};
+        System.out.println(sol.longestCommonPrefix(arr3, arr4)); // 0
+    }
+}
