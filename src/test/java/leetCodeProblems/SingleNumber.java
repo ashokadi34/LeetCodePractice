86c12c031880b78de39f828e77d70f443e9122da
ashokadi34 <ashokadi34@gmail.com>
2026-05-24 13:33:52 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/SingleNumber.java
@@ -0,0 +1,29 @@
+package leetCodeProblems;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int num : nums) {
+                sum += (num >> i) & 1;
+            }
+            sum %= 3;
+            if (sum != 0) {
+                ans |= (1 << i);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        SingleNumber sol = new SingleNumber();
+
+        int[] nums1 = {2, 2, 3, 2};
+        System.out.println(sol.singleNumber(nums1));  // 3
+
+        int[] nums2 = {0, 1, 0, 1, 0, 1, 99};
+        System.out.println(sol.singleNumber(nums2));  // 99
+    }
+}
