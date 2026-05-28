d4ba3efff5679538ae0d9ce85b2ca31989f308ee
ashokadi34 <ashokadi34@gmail.com>
2026-05-28 21:52:50 +0530

    new file added

---
+++ src/test/java/leetCodeProblems/HammingDistance.java
@@ -0,0 +1,14 @@
+package leetCodeProblems;
+
+public class HammingDistance {
+    public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+
+    public static void main(String[] args) {
+        HammingDistance sol = new HammingDistance();
+
+        System.out.println(sol.hammingDistance(1, 4)); // 2
+        System.out.println(sol.hammingDistance(3, 1)); // 1
+    }
+}
