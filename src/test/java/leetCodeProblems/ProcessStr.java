02e93a7f66d3f4efb797253bd6a3d77589dd51c6
ashokadi34 <ashokadi34@gmail.com>
2026-06-16 20:42:33 +0530

    new file

---
+++ src/test/java/leetCodeProblems/ProcessStr.java
@@ -0,0 +1,37 @@
+package leetCodeProblems;
+
+public class ProcessStr {
+
+    public String processStr(String s) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                result.append(c);
+            } else if (c == '*') {
+                if (result.length() > 0) {
+                    result.deleteCharAt(result.length() - 1);
+                }
+            } else if (c == '#') {
+                result.append(result.toString());
+            } else if (c == '%') {
+                result.reverse();
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        ProcessStr sol = new ProcessStr();
+
+        String s1 = "a#b%*";
+        System.out.println(sol.processStr(s1)); // ba
+
+        String s2 = "z*#";
+        System.out.println(sol.processStr(s2)); // ""
+
+        String s3 = "abc";
+        System.out.println(sol.processStr(s3)); // abc
+    }
+}
